adf1b3404269b1472db0927a76d01d33e3e27b7d
Décio Dias <ddiasweb@gmail.com>
2020-10-14 11:23:35 -0300

    Create test profile

--- src/test/java/com/nearestubs/nearestubs/NearestUbsApplicationTests.java
+++ src/test/java/com/nearestubs/nearestubs/NearestUbsApplicationTests.java
@@ -4,9 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace=Replace.ANY)
 public class NearestUbsApplicationTests {
 
 	@Test
